b5246371b248ebc7aa44b7baaad16a03c097ca8a
Prabal Gupta <er.prabalgupta.2411@gmail.com>
2024-12-03 19:27:59 +0530

    deleted:    src/app/api/completion/page.tsx

--- src/app/api/completion/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Completion() {
-    return (
-      <div className="p-4 text-center">
-        <h1 className="text-3xl font-bold">Test Completed!</h1>
-        <p>Thank you for taking the test.</p>
-      </div>
-    );
-  }
-  
